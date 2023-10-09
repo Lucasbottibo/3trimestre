be75c16b056bbe89889c4637a50a1a3716b0bc10
lucasbottino <113519542+Lucasbottibo@users.noreply.github.com>
2023-10-09 12:43:28 -0300

    09/10

---
+++ velocidade.js
@@ -0,0 +1,19 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+//ouvinte
+
+frm = addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const permitida = Number(frm.inPermitida.value)
+    const condutor = Number(frm.inCondutor.value)
+    let leve = permitida + (permitida * 0.2)
+
+    if(condutor <= permitida){
+        resp.innerText = `Situação: Sem Multa`
+
+    }else if(condutor <= leve){
+        
+    }
+})
