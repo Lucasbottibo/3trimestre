efad3f8cabf30094147486cdd1abc59fb6735ca7
lucasbottino <113519542+Lucasbottibo@users.noreply.github.com>
2023-10-18 12:32:02 -0300

    18/11

--- triangulo.js
+++ triangulo.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form")
-const resp = document.querySelector("outResp")
-const resp1 = document.querySelector("outResp1")
+const resp = document.querySelector("#outResp")
+const resp1 = document.querySelector("#outResp1")
 
 //ouvinte
 form.addEventListener("submit", (e)=>{
@@ -10,7 +10,7 @@ form.addEventListener("submit", (e)=>{
     const ladoB = Number(form.inLadoB.value)
     const ladoC = Number(form.inLadoC.value)
 
-    if((ladoA > ladoB + ladoC) || (ladoB > ladoA + ladoc) || (ladoC > ladoA + ladoB)){
+    if((ladoA > ladoB + ladoC) || (ladoB > ladoA + ladoC) || (ladoC > ladoA + ladoB)){
         alert("Essas medidas não podem formar um triângulo. Um dos lados não pode ser maior que a soma dos outros dois.")
         form.inLadoA.focus()
         form.inLadoB.focus()
@@ -20,7 +20,11 @@ form.addEventListener("submit", (e)=>{
 
     resp.innerText = `Lados podem formar um triângulo`
 
-    if(ladoA == ladoB && ladoA == ladoC){
-        
+    if(ladoA == ladoB && ladoA == ladoC){                                                   //três lados são iguais
+        resp1.innerText = `Tipo: Equilátero`
+    } else if((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC)){                    //dois lados são iguais
+        resp1.innerText = `Tipos: Isóceles`
+    }else{
+        resp1.innerText = `Tipo: Escaleno`                                                  //três lados diferentes
     }
 })
